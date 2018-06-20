f72ffe969365f7739f3f43c0beaad9f726d21237
Vincent <vleclerc@inow.fr>
2018-06-20 12:03:00 +0200

    Add masterpage and remove mainpage

--- Techem/Techem/App.xaml.cs
+++ Techem/Techem/App.xaml.cs
@@ -1,4 +1,5 @@
 using System;
+using Techem.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace Techem
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new MasterPage();
 		}
 
 		protected override void OnStart ()
